12c50fba015896ead670f6fbb0856681343c2a07
Aaron Gerig <a.gerig@w-vision.ch>
2019-04-17 16:32:02 +0200

    finished the course

--- lesson/5/model.js
+++ lesson/5/model.js
@@ -1,6 +1,9 @@
 export default class Model {
-  constructor(data = []) {
+  constructor(options = {}) {
+    const data = options.data || [];
+    delete options.data;
     this.$collection = [];
+    this.$options = Object.assign({ primaryKey: 'id' }, options);
 
     if (data.length) {
       this.record(data);
@@ -8,13 +11,12 @@ export default class Model {
   }
 
   record(data) {
-    const primaryKey = 'id';
     const mappedData = data.map(entry => {
-      if (entry[primaryKey]) {
+      if (entry[this.$options.primaryKey]) {
         return entry;
       }
 
-      entry[primaryKey] = Date.now();
+      entry[this.$options.primaryKey] = Date.now();
 
       return entry;
     });
@@ -26,14 +28,19 @@ export default class Model {
     return this.$collection.map(entry => Object.assign({}, entry));
   }
 
-  find(value) {
-    const primaryKey = 'id';
-    const entry = this.$collection.find(entry => entry[primaryKey] === value);
+  find(key) {
+    const entry = this.$collection.find(entry => entry[this.$options.primaryKey] === key);
 
     return entry ? Object.assign({}, entry) : null;
   }
 
-  update() {
+  update(key, data) {
+    const index = this.$collection.findIndex(entry => entry[this.$options.primaryKey] === key);
 
+    if (index < 0) {
+      return false;
+    }
+
+    this.$collection.splice(index, 1, Object.assign(this.$collection[index], data));
   }
 }
--- lesson/5/model.spec.js
+++ lesson/5/model.spec.js
@@ -1,12 +1,22 @@
 import Model from './model';
 
+function createModel(data = [], options = {}) {
+  return new Model({
+    ...options,
+    data,
+  });
+}
+
 test('new works', () => {
-  expect(new Model).toBeInstanceOf(Model);
+  expect(createModel()).toBeInstanceOf(Model);
 });
 
 test('model structure', () => {
-  expect(new Model).toEqual(expect.objectContaining({
+  expect(createModel()).toEqual(expect.objectContaining({
     $collection: expect.any(Array),
+    $options: expect.objectContaining({
+      primaryKey: 'id',
+    }),
     record: expect.any(Function),
     all: expect.any(Function),
     find: expect.any(Function),
@@ -14,18 +24,27 @@ test('model structure', () => {
   }));
 });
 
+describe('customizations', () => {
+  test('we can customize the primaryKey', () => {
+    const model = createModel([], {
+      primaryKey: 'name',
+    });
+    expect(model.$options.primaryKey).toBe('name');
+  });
+});
+
 describe('record', () => {
   const heroes = [{ id: 1, name: 'Batman' }, { name: 'Black Panther' }];
 
   test('can add data to the collection', () => {
-    const model = new Model();
+    const model = createModel();
     model.record(heroes);
     expect(model.$collection).toEqual([heroes[0], { id: expect.any(Number), name: heroes[1].name }]);
   });
 
   test('gets called when data is passed to Model', () => {
     const spy = jest.spyOn(Model.prototype, 'record');
-    new Model(heroes);
+    createModel(heroes);
     expect(spy).toHaveBeenCalled();
     spy.mockRestore();
   });
@@ -33,17 +52,17 @@ describe('record', () => {
 
 describe('all', () => {
   test('returns empty model', () => {
-    const model = new Model();
+    const model = createModel();
     expect(model.all()).toEqual([]);
   });
 
   test('returns model data', () => {
-    const model = new Model([{ name: 'Batman' }, { name: 'Joker' }]);
+    const model = createModel([{ name: 'Batman' }, { name: 'Joker' }]);
     expect(model.all().length).toBe(2);
   });
 
   test('original data stays intact', () => {
-    const model = new Model([{ name: 'Batman' }]);
+    const model = createModel([{ name: 'Batman' }]);
     const data = model.all();
     data[0].name = 'Joker';
 
@@ -55,12 +74,39 @@ describe('find', () => {
   const heroes = [{ id: 1, name: 'Batman' }, { id: 2, name: 'Black Panther' }];
 
   test('returns null if nothing matches', () => {
-    const model = new Model();
+    const model = createModel();
     expect(model.find(1)).toEqual(null);
   });
 
   test('find returns a matching entry', () => {
-    const model = new Model(heroes);
+    const model = createModel(heroes);
     expect(model.find(1)).toEqual(heroes[0]);
+  });
+});
+
+describe('update', () => {
+  const heroesAndVillains = [{ id: 1, name: 'Batman' }];
+  let model;
+
+  beforeEach(() => {
+    const dataset = JSON.parse(JSON.stringify(heroesAndVillains));
+    model = createModel(dataset);
+  });
+
+  test('an entry by id', () => {
+    model.update(1, { name: 'Joker' });
+    expect(model.find(1).name).toBe('Joker');
+  });
+
+  test('extend an entry by id', () => {
+    model.update(1, { cape: true });
+    expect(model.find(1)).toEqual(expect.objectContaining({
+      name: 'Batman',
+      cape: true,
+    }));
+  });
+
+  test('return false if no entry matches', () => {
+    expect(model.update(2, {})).toBe(false);
   });
 });
